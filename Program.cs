35e93413577af02e55f84aafbe4df5d73e29e5ae
ShykiFr <moh.dzaki.ayatillah@gmail.com>
2026-04-14 07:49:10 +0700

    menambahkan kelas Watchlist_103022400090

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
-﻿public class Program
+﻿using Modul7_BNP;
+
+public class Program
 {
     public static void Main(string[] args)
     {
-        FilmFavorit_103022400090 film = new FilmFavorit_103022400090();
-        film.readJSON();
+        //FilmFavorit_103022400090 film = new FilmFavorit_103022400090();
+        //film.readJSON();
+        Watchlist_103022400090 watchlist = new Watchlist_103022400090();
+        watchlist.readJSON();
     }
 }
---
+++ Watchlist_103022400090.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace Modul7_BNP
+{
+    internal class Watchlist_103022400090
+    {
+        public string watchlistName { get; set; }
+        public string createdBy { get; set; }
+        public string id { get; set; }
+        public string title { get; set; }
+        public int year { get; set; }
+        public string genre { get; set; }
+        public float rating { get; set; }
+        public List<Watchlist_103022400090> movies { get; set; }
+
+        public void readJSON()
+        {
+            string json = File.ReadAllText("jurnal7_2_103022400090.json");
+            Watchlist_103022400090 watchlist = JsonSerializer.Deserialize<Watchlist_103022400090>(json);
+            Console.WriteLine($"Watchlist Name: {watchlist.watchlistName}\nCreated By: {watchlist.createdBy}\nMovies:");
+
+            foreach (var item in watchlist.movies)
+            {
+                Console.WriteLine($"{item.id} {item.title} ({item.year} - {item.rating})");
+            }
+
+        }
+    }
+}
