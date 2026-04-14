8530c42a8cea7dc65673a1115e43260eda1c0257
JustAHermit <arsall1202@gmail.com>
2026-04-14 07:43:59 +0700

    menambahkan class FilmFavorit_103022430010

---
+++ Jurnal7_4802/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string jsonFile1 = "D:\\grw cwAERA\\Jurnal7_4802\\jurnal7_1_103022430010.json";
+
+        FilmFavorit_103022430010 ff = new FilmFavorit_103022430010();
+        ff.ReadJSON(jsonFile1);
+
+    }
+}
