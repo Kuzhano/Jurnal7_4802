13dccd68f07c10518421267b4a22253abec78897
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2026-04-14 07:39:02 +0700

    commit message

---
+++ FilmFavorit_103022400020.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.JavaScript;
+using System.Text;
+using System.Text.Json;
+
+namespace Modul7_BNP
+{
+    internal class FilmFavorit_103022400020
+    {
+        public string title { get; set; }
+        public string director { get; set; }
+        public string year { get; set; }
+        public string genre { get; set; }
+        public int rating { get; set; }
+        public int genredurationMinutes { get; set; }
+        public bool isWatched { get; set; }
+
+        public void ReadJSON()
+        {
+            string json = File.ReadAllText("jurnal7_1_103022400020.json");
+            var film = JsonSerializer.Deserialize<FilmFavorit_103022400020>(json);
+            Console.WriteLine($"Judul {film.title}, director {film.director}, tahun {film.year}, genre {film.genre}, rating {film.rating}" +
+                $", durasi {film.genredurationMinutes}, Sudah ditonton? {film.isWatched}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Modul7_BNP
+{ 
+    class main
+    {
+        static void Main(String[] args) { 
+            FilmFavorit_103022400020 film = new FilmFavorit_103022400020();
+
+            film.ReadJSON();
+        }
+    }
+}
