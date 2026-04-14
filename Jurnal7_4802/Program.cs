daef1cadac1fbe7bf7e228d75f3daa5fa3fb8c5e
JustAHermit <arsall1202@gmail.com>
2026-04-14 07:57:04 +0700

    menambahkan class watchlist_103022430010

--- Jurnal7_4802/Program.cs
+++ Jurnal7_4802/Program.cs
@@ -7,5 +7,19 @@
         FilmFavorit_103022430010 ff = new FilmFavorit_103022430010();
         ff.ReadJSON(jsonFile1);
 
+        Console.WriteLine("");
+
+        string jsonFile2 = "D:\\grw cwAERA\\Jurnal7_4802\\jurnal7_2_103022430010.json";
+
+        Watchlist_103022430010 wl = new Watchlist_103022430010();
+        wl.ReadJSON(jsonFile2);
+
+        Console.WriteLine("");
+
+        string jsonFile3 = "D:\\grw cwAERA\\Jurnal7_4802\\jurnal7_3_103022430010.json";
+
+        FilmFavorit_103022430010 ff = new FilmFavorit_103022430010();
+        ff.ReadJSON(jsonFile1);
+
     }
 }
---
+++ Jurnal7_4802/WatchList_103022430010.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+class Watchlist_103022430010
+{
+    public string watchlistName { get; set; }
+    public string createdBy { get; set; }
+    public string id { get; set; }
+    public string title { get; set; }
+    public int year { get; set; }
+    public string genre { get; set; }
+    public float rating { get; set; }
+    public List<Watchlist_103022430010> movies { get; set; }
+
+    public void ReadJSON(string jsonString)
+    {
+        try
+        {
+            jsonString = File.ReadAllText(jsonString);
+
+            var wl = JsonSerializer.Deserialize<Watchlist_103022430010>(jsonString);
+
+            Console.WriteLine($"Watchlist Name: {wl.watchlistName}\nCreated By: {wl.createdBy}\nMovies:");
+
+            foreach (var item in wl.movies)
+            {
+                Console.WriteLine($"{item.id} {item.title} ({item.year} - {item.rating})");
+            }
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("Error: The JSON file was not found.");
+        }
+    }
+}
