e07ab43ebc5cfa081d7f04a33aceb24f5529f7d7
ShykiFr <moh.dzaki.ayatillah@gmail.com>
2026-04-14 07:30:30 +0700

    Menambahkan kelas FilmFavorit_103022400090

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        FilmFavorit_103022400090 film = new FilmFavorit_103022400090();
+        film.readJSON();
+    }
+}
